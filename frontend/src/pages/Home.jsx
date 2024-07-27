40ba669bdf5757d16fc75bf68b40ce603e0c360b
wankoon <puah.wankoon@asiaone.com>
2024-07-27 19:05:24 +0800

    testing the path

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,26 @@
-import CreateProfile from "./CreateProfile"
+import { Link } from "react-router-dom";
+import CreateProfile from "./CreateProfile";
 
 const Home = () => {
   return (
     <>
       <div className="bg-banner bg-center bg-cover h-screen flex justify-center items-center">
-        <div className='wrapper-header'>
-          <p className='text-8xl text-white font-bold font-header'>Code Cave</p>
+        <div className="wrapper-header">
+          <p className="text-8xl text-white font-bold font-header">Code Cave</p>
           <hr />
-          <p className='float-center p-2 text-yellow-400 text-2xl text-white font-header text-center'>Where Logic Meets Imagination</p>
-          <div className='flex gap-2 justify-center'>
-            <a className="bg-black p-2 font-bold text-white" href="/profiles">Show All</a>
-            <a className="bg-black p-2 font-bold text-white" href="#form">Create One</a>
+          <p className="float-center p-2 text-yellow-400 text-2xl text-white font-header text-center">
+            Where Logic Meets Imagination
+          </p>
+          <div className="flex gap-2 justify-center">
+            <Link
+              className="bg-black p-2 font-bold text-white"
+              href="/profiles"
+            >
+              Show All
+            </Link>
+            <a className="bg-black p-2 font-bold text-white" href="#form">
+              Create One
+            </a>
           </div>
         </div>
       </div>
@@ -18,7 +28,7 @@ const Home = () => {
         <CreateProfile />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
